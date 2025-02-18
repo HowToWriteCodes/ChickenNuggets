7eaffd95c63eb94d8c14701dff7a3223e81ad036
Nikhil Yadav <nikhilyadav1140@gmail.com>
2025-02-18 22:39:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,17 @@
-// server.js
-import { config } from 'dotenv';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import express from "express";
 import { rankCache } from "./scripts/rankService.js";
 import { historyCache } from "./scripts/historyService.js";
-
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-config({ path: '.env' });
-
-import express from "express";
 import { PORT } from "./config.js";
 import { fetchPlayerData } from "./scripts/rankService.js";
 import { processLastFiveGames } from "./scripts/historyService.js";
+import { updateData } from './scripts/updateService.js';
 
 const app = express();
-
-const CACHE_TTL = 35 * 60 * 1000; // 35 minutes in milliseconds
-
-
 
 app.get("/", async(req,res) => {
   res.status(200).send("ChickenNuggets ChickenNuggets");
 });
 
-// Add this to index.js
 app.get("/debug/cache", (req, res) => {
   const rankStats = rankCache.getStats();
   const historyStats = historyCache.getStats();
@@ -35,6 +20,14 @@ app.get("/debug/cache", (req, res) => {
       rank: rankStats,
       history: historyStats
   });
+});
+
+app.get("/update/:uid", async (req, res) => {
+  try {
+    await updateData(req.params.uid);
+  } catch (error) {
+    res.status(500).send("500");
+  }
 });
 
 app.get("/rank/:uid", async (req, res) => {

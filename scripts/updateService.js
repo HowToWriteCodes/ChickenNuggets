21ffd9516b799c8b44844c4a72d31483d9214f88
Nikhil Yadav <nikhilyadav1140@gmail.com>
2025-02-18 22:40:19 +0530

    Update updateService.js

--- scripts/updateService.js
+++ scripts/updateService.js
@@ -1,12 +1,10 @@
 /* Simple service to update data
-Could setup a way to avoid frequent updates
-By checking last_history_update before hitting this service
-Has a 30 min delay before new data is updated*/
+Has a 30 min delay before new data is actually updated
+So don't bother hitting it every now and then*/
 
 import axios from "axios";
 import { apiConfig } from "../config.js";
 
 export async function updateData(UID){
-    const url = `https://marvelrivalsapi.com/api/v1/player/${UID}/update`;
-    return await axios.request({ ...apiConfig, url});
+    return await axios.request({ ...apiConfig, url:`/${UID}/update`});
 }

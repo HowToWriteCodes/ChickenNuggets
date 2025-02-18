5102717b586a0eee6ed822acfe8f08119cfa4d85
Nikhil Yadav <nikhilyadav1140@gmail.com>
2025-02-18 22:47:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Just some random config need not worry about
 export const PORT = process.env.PORT || 3000;
 export const API_KEY = process.env.API_KEY;
-const CACHE_TIMEOUT = 35 * 60 * 1000; // Recommended: 35 minutes, Min: 30 minutes
+export const CACHE_TIMEOUT = 35 * 60 * 1000; // Recommended: 35 minutes, Min: 30 minutes
 
 export const apiConfig = {
   method: "get",

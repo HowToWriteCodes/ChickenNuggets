67a54d56a7ae0e159920813fbc5b0a808c93602f
Nikhil Yadav <nikhilyadav1140@gmail.com>
2025-02-18 22:39:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,14 @@
-// config.js
+// Just some random config need not worry about
 export const PORT = process.env.PORT || 3000;
 export const API_KEY = process.env.API_KEY;
-export const CACHE_TTL = 300000; // 5 minutes in milliseconds
+const CACHE_TIMEOUT = 35 * 60 * 1000; // Recommended: 35 minutes, Min: 30 minutes
 
 export const apiConfig = {
   method: "get",
   url: "",
+  baseURL:"https://marvelrivalsapi.com/api/v1/player",
   headers: {
     "x-api-key": API_KEY,
   },
 };
+
